9e9c88dc3bd12cf0523cd05139c364d1f720d23d
Mykola Savenok <savenok.mikolay@gmail.com>
2024-02-01 15:08:22 +0200

    correct server.mjs

--- server/server/server.mjs
+++ server/server/server.mjs
@@ -6,26 +6,25 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-//Створення Fastify сервера
+
 const server = fastify();
 
-//Обробка cors запитів
+
 server.register(cors, {});
-//Шлях до кореневої директорії
+
 server.register(fastifyStatic, {
    root: path.join(__dirname, "../../dist"),
 });
-//Обробник маршруту для HTTP GET-запитів
+
 server.get('/', (_, reply) => {
    reply.sendFile('index.html')
 });
 
-
-const port = process.env.PORT || 5556;
+const port = process.env.PORT || 9999;
 const host = process.env.HOST || "localhost";
 
 server
-   .listen( { port, host })
+   .listen({ port, host })
    .then(() => {
       console.log("Server is running");
    })
