6723b8de857ff7c0a4760e8ca40b7318de4c847f
Mykola Savenok <savenok.mikolay@gmail.com>
2024-02-06 16:27:14 +0200

    add block

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -7,20 +7,24 @@ const About = () => {
          <div className="about-us">
             <div className="container">
                <div className="about-us__row">
-                  <p className="about-us__text">The best smoothie bar on the planet earth.<br /> If it's a smoothie, it's only us.</p>
+                  <p className="about-us__text">Congratulations! You made the right choice, it couldn't have been any other way. We guess your wishes and make your holiday special. It's time to call, introduce ourselves and tell you why it took us so long to find each other.</p>
                   <div className="about-us__image"><img src="/static/images/aboutUs/smuzi.jpg" alt="smuzi" /></div>
                </div>
                <div className="contacts">
-                  <h2 className="contacts__header">Contacts</h2>
-                  <p className="contacts__text">Congratulations! You made the right choice, it couldn't have been any other way. We guess your wishes and make your holiday special. It's time to call, introduce ourselves and tell you why it took us so long to find each other.</p>
-                  <address className="contacts__info">
-                     <p>c. Kiyv</p>
-                     <p>street Khreshchatyk 1</p>
-                     <div className="contacts__number">
-                        <a href="tel:+380677777777">Tel: +380677777777</a>
-                        <a href="tel:+380670000000">Tel: +380670000000</a>
+                  <div className="contacts__body">
+                     <div className="contacts__img"><img src="/static/images/aboutUs/AboutMap.png" alt="maps" /></div>
+                     <div className="contacts__content">
+                        <h2 className="contacts__title">Contacts</h2>
+                        <address className="contacts__info">
+                           <p>c. Kiyv</p>
+                           <p>street Khreshchatyk, 1</p>
+                           <div className="contacts__number">
+                              <a href="tel:+380677777777">Tel: +380677777777</a>
+                              <a href="tel:+380670000000">Tel: +380930000000</a>
+                           </div>
+                        </address>
                      </div>
-                  </address>
+                  </div>
                </div>
             </div>
          </div>
