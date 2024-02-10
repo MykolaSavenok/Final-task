dddf557613f84d0b7f01fa80c6e456b3df6fc5da
Mykola Savenok <savenok.mikolay@gmail.com>
2024-02-10 16:43:35 +0200

    correct settings

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Fullscreen from '../Main-fullscreen/Fullscreenblock.jsx'
 import MenuSmoothies from "../MenuSmoothies/MenuSmoothies.jsx";
-import WeAre from "../weAre/weAre.jsx";
+import WeAre from "../WeAre/WeAre";
 import Benefits from "../Benefits/Benefits.jsx";
 import SpecaialOffer from "../SpecaialOffer/SpecaialOffer.jsx";
 
