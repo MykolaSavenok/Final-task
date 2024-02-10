29c91cec41be39403ab2c3f5450e234ee96d82d9
Mykola Savenok <savenok.mikolay@gmail.com>
2024-02-10 17:04:04 +0200

    optimized css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
    entry: './client/src/index.jsx',
@@ -85,5 +86,8 @@ module.exports = {
             ],
          },
       ]
-   }
+   },
+   optimization: {
+      minimizer: [new CssMinimizerPlugin()],
+    },
 };
